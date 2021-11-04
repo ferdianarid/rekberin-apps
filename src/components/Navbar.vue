eed7ff0276760f2238c42d2c30da08367715d6ea
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-04 16:15:05 +0700

    navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,18 @@
 <template>
-       <div class="navbar row container">
-              <div class="brand">
-                     <img src="../assets/rekberin.png" alt="">
+       <div class="navbar row container pl-28 pr-28 pt-5 flex justify-between items-center">
+              <!-- Brand -->
+              <div class="brand flex items-center ">
+                     <img src="../assets/images/circle.png" alt="rekberin" width="48">
+                     <h1 class="pl-2 font-bold">Rekberin</h1>
+              </div>
+              <!-- Navigation -->
+              <div class="navigation flex items-center justify-arround ml-auto mr-3">
+                     <li class="mr-5"><a href="#">Homepage</a></li>
+                     <li><a href="#">Classroom</a></li>
+              </div>
+              <div class="mainButton">
+                     <button class="login p-3 text-md text-green-600 rounded-md bg-transparent">Login</button>
+                     <button class="register ml-3 p-3 text-md bg-green-600 rounded-md text-white">Register</button>
               </div>
        </div>
 </template>
@@ -12,4 +23,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+/* navigation */
+.navigation li {
+       list-style: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ import App from './App.vue'
 // import tailwind css
 import "../src/assets/stylesheets/index.css"
 
+// import global css
+import "../src/assets/stylesheets/main.css"
+
 createApp(App).mount('#app')
