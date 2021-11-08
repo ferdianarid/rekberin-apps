386947a1a939fc147bff4e471d4f1b49f04ebec4
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-08 13:51:06 +0700

    navbar dan header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
        <div>
               <div class="font-sans antialiased" id="app">
                      <!-- Class Navbar -->
-                     <nav class="navbar flex items-center justify-between flex-wrap pl-6 bg-transparent p-6 md:pl-24 pr-24">
+                     <nav class="navbar flex items-center justify-between flex-wrap pl-6 pr-6 bg-transparent p-6 md:pl-24 md:pr-24">
                             <!-- Brand and Logos -->
                             <div class="flex items-center flex-no-shrink mr-6">
                                    <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -4,20 +4,20 @@
        <!-- Navbar Component -->
        <Navbar />
        <!-- Container -->
-       <div class="px-8 md:pl-24 pr-24">
+       <div class="mt-5 px-8 md:pl-24 md:pr-24 md:mt-1">
        <!-- Header Section -->
-       <header class="row flex h-auto">
+       <header class="row md:flex h-auto">
               <!-- Heading -->
-              <div class="w-full md:w-1/2 pt-20 pb-10 mb-36">
-                     <h1 class="text-5xl font-extrabold leading-tight pb-3">Transfer Beda Bank Bebas Biaya Admin</h1>
-                     <p class="pr-16 leading-6">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, facere? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+              <div class="w-full md:w-1/2 md:pt-10 md:pb-10 md:mb-36">
+                     <h1 class="title-heading text-4xl pt-3 md:text-5xl font-extrabold md:leading-tight md:pb-3">Transfer Beda Bank Bebas Biaya Admin</h1>
+                     <p class="pt-5 md:pr-16 md:leading-7 md:pt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, facere? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
                      <!-- Button Call to Action -->
-                     <button class="py-3 px-16 bg-green-800 rounded-md mt-4">
+                     <button class="px-14 bg-green-800 py-2 mt-5 rounded-md mb-5 md:py-3 md:px-16 md:bg-green-800 md:rounded-md md:mt-4 hover:bg-green-900">
                             <p class="text-lg text-white">Daftar</p>
                      </button>
               </div>
               <!-- Header Images -->
-              <div class="headerImages w-1/2">
+              <div class="mt-14 md:headerImages md:w-1/2">
                      <img src="../assets/images/header.png" alt="headingImages">
               </div>
        </header>
